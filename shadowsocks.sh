464fef552514f1ad6efcb5a6ef02b6c357b3fdfa
zhangguangxin1234 <zhangguangxin1234@126.com>
2021-05-31 11:28:13 +0800

    Update shadowsocks.sh

--- shadowsocks.sh
+++ shadowsocks.sh
@@ -4,3 +4,9 @@ snap install shadowsocks-libev
 ln -s /var/lib/snapd/snap /snap
 sleep 10 
 snap install shadowsocks-libev
+cp ./config.json /snap/bin/config.json
+cp ./ss.service /lib/systemd/system/ss.service
+systemctl start ss.service
+systemctl stop firewalld
+systemctl enable ss.service
+systemctl status ss.service
