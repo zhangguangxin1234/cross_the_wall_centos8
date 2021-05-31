f01154e2a8c34e1f548ee26184ba978a3a457b07
zhangguangxin1234 <zhangguangxin1234@126.com>
2021-05-31 11:53:56 +0800

    Update shadowsocks.sh

--- shadowsocks.sh
+++ shadowsocks.sh
@@ -1,12 +1,16 @@
 dnf install epel-release -y
+
 dnf install snapd
-snap install shadowsocks-libev
 systemctl enable --now snapd.socket
+sleep 10 
 ln -s /var/lib/snapd/snap /snap
 sleep 10 
+
 snap install shadowsocks-libev
+
 cp ./config.json /snap/bin/config.json
 cp ./ss.service /lib/systemd/system/ss.service
+
 systemctl start ss.service
 systemctl stop firewalld
 systemctl enable ss.service
